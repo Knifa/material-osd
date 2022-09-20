3ca3465d384e39e578bed56fa9dffad224167a2c
Daniel Callander <knifacat@gmail.com>
2022-09-20 22:49:51 +0100

    Bunch more icons, etc.

--- gen.py
+++ gen.py
@@ -1,4 +1,5 @@
-from typing import cast
+import functools
+from typing import cast, Callable
 import dataclasses
 import math
 
@@ -17,9 +18,10 @@ TILE_HEIGHT = 18 * 3
 TILES_PER_ROW = 16
 TILE_ROW_COUNT = math.ceil(len(CHARS) / TILES_PER_ROW)
 
+FONT_PATH = "fonts/BebasNeue-Regular.ttf"
 FONT = ImageFont.truetype(
     # "fonts/roboto/RobotoMono-Regular.ttf",
-    "fonts/BebasNeue-Regular.ttf",
+    FONT_PATH,
     math.floor(TILE_HEIGHT * 0.85),
 )
 
@@ -31,6 +33,7 @@ class IconImage:
     index: int
     material_icon: str
     with_background: bool = True
+    scale: float = 1.0
 
 
 @dataclasses.dataclass
@@ -38,25 +41,107 @@ class IconEmpty:
     index: int
 
 
+@dataclasses.dataclass
+class IconCustom:
+    index: int
+    generator: Callable[[], Image.Image]
+
+
+@dataclasses.dataclass
+class IconText:
+    index: int
+    text: str
+    center: bool = False
+    scale: float = 0.5
+
+    def generate(self):
+        img = Image.new(
+            "RGBA",
+            (TILE_WIDTH, TILE_HEIGHT),
+            (0, 0, 0, 0),
+        )
+        img_draw = ImageDraw.Draw(img)
+
+        font = ImageFont.truetype(
+            FONT_PATH,
+            math.floor(TILE_HEIGHT * self.scale),
+        )
+
+        anchor = "ms" if self.center else "ls"
+
+        text_bbox = img_draw.textbbox(
+            (0, 0),
+            self.text,
+            font=font,
+            anchor=anchor,
+            spacing=0,
+        )
+
+        text_xy = (
+            (TILE_WIDTH / 2) if self.center else TILE_WIDTH * 0.1,
+            (TILE_HEIGHT * 0.8) - text_bbox[3],
+        )
+
+        img_draw.multiline_text(
+            text_xy,
+            self.text,
+            font=font,
+            anchor=anchor,
+            spacing=0,
+            fill="white",
+        )
+
+        euclid = euclidean_distance_transform(img.getchannel("A"))
+        img = add_outline_via_euclidean_distance_transform(img, euclid)
+
+        return img
+
+
 ICONS = [
-    IconImage(1, "signal"),
+    IconImage(1, "signal", scale=1.2),  # RSSI
     IconImage(2, "left"),
     IconImage(3, "right"),
-    IconEmpty(6),
-    IconImage(60, "left"),
-    IconImage(62, "right"),
+    IconText(6, "v"),  # Voltage Unit
+    IconText(7, "mah", scale=0.4),  # mAh
+    IconText(12, "m"),  # Alt Unit (m)
+    IconText(13, "°f"),  # Temp Unit (F)
+    IconText(14, "°c"),  # Temp Unit (C)
+    IconText(15, "ft"),  # Alt Unit (ft)
+    IconText(20, "rol", center=True),  # "Roll" ???  TODO: Graphic?
+    IconText(21, "pit", center=True),  # "Pitch" ???  TODO: Graphic?
+    # Cardinal Directions (NSEW)
+    IconText(24, "N"),
+    IconText(25, "S"),
+    IconText(26, "E"),
+    IconText(27, "W"),
+    IconImage(60, "left"),  # Override < Char
+    IconImage(62, "right"),  # Override > Char
+    # Crosshair (3x1)
     IconEmpty(114),
     IconImage(115, "crosshair", False),
     IconEmpty(116),
-    IconImage(123, "signal"),
-    IconImage(144, "battery_full"),
-    IconImage(145, "battery_5_bar"),
-    IconImage(146, "battery_4_bar"),
-    IconImage(147, "battery_3_bar"),
-    IconImage(148, "battery_2_bar"),
-    IconImage(149, "battery_1_bar"),
-    IconImage(150, "battery_0_bar"),
-    IconImage(151, "battery_alert"),
+    IconImage(123, "signal", scale=1.2),  # RSSI (LQ)
+    IconText(125, "km"),  # Dist Unit (km)
+    IconText(126, "mi"),  # Dist Unit (mi)
+    IconText(127, "alt", center=True),  # "Alt" ???
+    IconText(137, "lat", center=True),  # "Lat" ???
+    # Battery
+    IconImage(144, "battery_full", scale=1.25),
+    IconImage(145, "battery_5_bar", scale=1.25),
+    IconImage(146, "battery_4_bar", scale=1.25),
+    IconImage(147, "battery_3_bar", scale=1.25),
+    IconImage(148, "battery_2_bar", scale=1.25),
+    IconImage(149, "battery_1_bar", scale=1.25),
+    IconImage(150, "battery_0_bar", scale=1.25),
+    IconImage(151, "battery_alert", scale=1.25),
+    IconText(152, "lon", center=True),  # "Lon" ???
+    IconText(153, "ft/s", scale=0.4),  # Speed Unit (ft/s)
+    IconText(154, "a"),  # Amps Unit
+    IconText(155, "on\nmn", scale=0.4),  # Timer (On) TODO: Clock graphic?
+    IconText(156, "fly\nmn", scale=0.4),  # Timer (On) TODO: Drone clock graphic?
+    IconText(157, "mph", scale=0.4),  # Speed Unit (mph)
+    IconText(158, "kph", scale=0.4),  # Speed Unit (kph)
+    IconText(159, "m/s", scale=0.4),  # Speed Unit (m/s)
 ]
 
 
@@ -69,13 +154,12 @@ def get_path_to_material_icon(
 def get_icon_image(icon: IconImage) -> Image.Image:
     if isinstance(icon, IconEmpty):
         return Image.new("RGBA", (TILE_WIDTH, TILE_HEIGHT), (0, 0, 0, 0))
+    elif isinstance(icon, IconText):
+        return icon.generate()
+    elif isinstance(icon, IconCustom):
+        return icon.generator()
 
-    out_img = Image.new(
-        "RGBA",
-        (TILE_WIDTH, TILE_HEIGHT),
-        (0, 0, 0, 0)
-        # BACKGROUND if icon.with_background else (0, 0, 0, 0),
-    )
+    out_img = Image.new("RGBA", (TILE_WIDTH, TILE_HEIGHT), (0, 0, 0, 0))
 
     icon_path = get_path_to_material_icon(icon.material_icon)
 
@@ -93,22 +177,29 @@ def get_icon_image(icon: IconImage) -> Image.Image:
 
     icon_img = icon_img.resize(
         (
-            TILE_WIDTH,
-            TILE_WIDTH,
+            int(TILE_WIDTH * icon.scale),
+            int(TILE_WIDTH * icon.scale),
         ),
         resample=Image.Resampling.BILINEAR,
     )
 
+    scale_width_diff = TILE_WIDTH - icon_img.width
+    scale_height_diff = TILE_HEIGHT - icon_img.height
+
+    paste_x = scale_width_diff // 2
+    paste_y = scale_height_diff // 2
+
     out_img.paste(
         icon_img,
-        (0, (TILE_HEIGHT // 2) - (TILE_WIDTH // 2)),
+        (paste_x, paste_y),
         icon_img,
     )
 
-    euclid = euclidean_distance_transform(out_img.getchannel("A"))
-    outline = add_outline_via_euclidean_distance_transform(out_img, euclid)
+    if icon.with_background:
+        euclid = euclidean_distance_transform(out_img.getchannel("A"))
+        out_img = add_outline_via_euclidean_distance_transform(out_img, euclid)
 
-    return outline
+    return out_img
 
 
 def get_char_image() -> Image.Image:
@@ -146,6 +237,7 @@ def get_char_image() -> Image.Image:
 def add_outline_via_euclidean_distance_transform(
     img: Image.Image, euclid: Image.Image, radius=3
 ) -> Image.Image:
+    return img
     img = img.copy()
     outline = Image.new("L", img.size, 0)
 
@@ -196,6 +288,7 @@ OCTAGON_PATTERN_9x9 = [
 
 
 def euclidean_distance_transform(img: Image.Image) -> Image.Image:
+    return img
     img = Image.eval(img, lambda x: 255 if x > 100 else 0)
     ed_img = Image.new("L", img.size, 0)
 
@@ -253,10 +346,12 @@ def main():
 
         out.paste(icon_img, (tile_x, tile_y))
 
-    # overlay.paste(out, (0, 0), out)
-    # out = overlay.copy()
+    preview = out.copy()
+    overlay.paste(preview, (0, 0), preview)
+    preview = overlay
 
-    out.save("text.png")
+    out.save("template.png")
+    preview.save("preview.png")
 
 
 if __name__ == "__main__":
