939d167333705b123a8ef0ce467f1b772d71d186
Daniel Callander <knifacat@gmail.com>
2022-09-20 23:57:20 +0100

    SVG support + more icons

--- gen.py
+++ gen.py
@@ -1,7 +1,9 @@
-import functools
 from typing import cast, Callable
 import dataclasses
 import math
+import cairosvg
+import io
+
 
 from PIL import (
     Image,
@@ -34,6 +36,104 @@ class IconImage:
     material_icon: str
     with_background: bool = True
     scale: float = 1.0
+
+    def generate(self):
+        out_img = Image.new("RGBA", (TILE_WIDTH, TILE_HEIGHT), (0, 0, 0, 0))
+
+        icon_path = get_path_to_material_icon(self.material_icon)
+
+        icon_img = Image.open(cast(str, icon_path))
+        icon_img = icon_img.convert("RGBA")
+        icon_img_a = icon_img.getchannel("A")
+        icon_img_l = Image.new("L", icon_img.size, 255)
+        icon_img = Image.merge(
+            "LA",
+            (
+                icon_img_l,
+                icon_img_a,
+            ),
+        )
+
+        icon_img = icon_img.resize(
+            (
+                int(TILE_WIDTH * self.scale),
+                int(TILE_WIDTH * self.scale),
+            ),
+            resample=Image.Resampling.BILINEAR,
+        )
+
+        scale_width_diff = TILE_WIDTH - icon_img.width
+        scale_height_diff = TILE_HEIGHT - icon_img.height
+
+        paste_x = scale_width_diff // 2
+        paste_y = scale_height_diff // 2
+
+        out_img.paste(
+            icon_img,
+            (paste_x, paste_y),
+            icon_img,
+        )
+
+        if self.with_background:
+            euclid = euclidean_distance_transform(out_img.getchannel("A"))
+            out_img = add_outline_via_euclidean_distance_transform(out_img, euclid)
+
+        return out_img
+
+
+@dataclasses.dataclass
+class IconSvg:
+    index: int
+    name: str
+    scale: float = 1.0
+    with_background: bool = True
+
+    def generate(self):
+        out_img = Image.new("RGBA", (TILE_WIDTH, TILE_HEIGHT), (0, 0, 0, 0))
+
+        icon_path = f"./node_modules/@mdi/svg/svg/{self.name}.svg"
+        icon_img = self._load_svg(icon_path)
+
+        scale_width_diff = TILE_WIDTH - icon_img.width
+        scale_height_diff = TILE_HEIGHT - icon_img.height
+
+        paste_x = scale_width_diff // 2
+        paste_y = scale_height_diff // 2
+
+        out_img.paste(
+            icon_img,
+            (paste_x, paste_y),
+            icon_img,
+        )
+
+        if self.with_background:
+            euclid = euclidean_distance_transform(out_img.getchannel("A"))
+            out_img = add_outline_via_euclidean_distance_transform(out_img, euclid)
+
+        return out_img
+
+    @staticmethod
+    def _load_svg(path: str) -> Image.Image:
+        svg_png = cairosvg.svg2png(
+            url=path,
+            parent_width=TILE_WIDTH,
+            parent_height=TILE_WIDTH,
+        )
+        svg_png = io.BytesIO(cast(bytes, svg_png))
+        svg_img = Image.open(svg_png)
+
+        svg_img_a = svg_img.getchannel("A")
+        icon_img_l = Image.new("L", svg_img.size, 255)
+
+        icon_img = Image.merge(
+            "LA",
+            (
+                icon_img_l,
+                svg_img_a,
+            ),
+        )
+
+        return icon_img
 
 
 @dataclasses.dataclass
@@ -99,32 +199,41 @@ class IconText:
 
 ICONS = [
     IconImage(1, "signal", scale=1.2),  # RSSI
-    IconImage(2, "left"),
-    IconImage(3, "right"),
+    IconSvg(2, "chevron-left"),
+    IconSvg(3, "chevron-right"),
+    IconSvg(4, "speedometer"),
+    IconSvg(5, "home"),  # Home
     IconText(6, "v"),  # Voltage Unit
     IconText(7, "mah", scale=0.4),  # mAh
     IconText(12, "m"),  # Alt Unit (m)
     IconText(13, "°f"),  # Temp Unit (F)
     IconText(14, "°c"),  # Temp Unit (C)
     IconText(15, "ft"),  # Alt Unit (ft)
-    IconText(20, "rol", center=True),  # "Roll" ???  TODO: Graphic?
-    IconText(21, "pit", center=True),  # "Pitch" ???  TODO: Graphic?
+    IconSvg(20, "axis-z-rotate-clockwise"),  # "Roll" ???  TODO: Graphic?
+    IconSvg(21, "horizontal-rotate-clockwise"),  # "Pitch" ???  TODO: Graphic?
     # Cardinal Directions (NSEW)
     IconText(24, "N"),
     IconText(25, "S"),
     IconText(26, "E"),
     IconText(27, "W"),
-    IconImage(60, "left"),  # Override < Char
-    IconImage(62, "right"),  # Override > Char
+    # IconImage(60, "left"),  # Override < Char
+    # IconImage(62, "right"),  # Override > Char
+    IconSvg(112, "speedometer"),  # Speedometer
+    IconSvg(113, "rotate-left"),  # Anti-clockwise ????
     # Crosshair (3x1)
     IconEmpty(114),
-    IconImage(115, "crosshair", False),
+    IconSvg(115, "crosshairs-gps", False),
     IconEmpty(116),
+    IconSvg(117, "chevron-double-up"),  # Up Chevron
+    IconSvg(118, "chevron-double-down"),  # Down Chevron
+    IconSvg(119, "chevron-double-right"),  # Right Chevron
+    IconSvg(120, "chevron-double-left"),  # Left Chevron
+    IconSvg(122, "thermometer"),  # Thermometer
     IconImage(123, "signal", scale=1.2),  # RSSI (LQ)
     IconText(125, "km"),  # Dist Unit (km)
     IconText(126, "mi"),  # Dist Unit (mi)
-    IconText(127, "alt", center=True),  # "Alt" ???
-    IconText(137, "lat", center=True),  # "Lat" ???
+    IconSvg(127, "altimeter"),  # "Alt" ???
+    IconSvg(137, "latitude"),  # Longitude ("Lon")
     # Battery
     IconImage(144, "battery_full", scale=1.25),
     IconImage(145, "battery_5_bar", scale=1.25),
@@ -134,11 +243,11 @@ ICONS = [
     IconImage(149, "battery_1_bar", scale=1.25),
     IconImage(150, "battery_0_bar", scale=1.25),
     IconImage(151, "battery_alert", scale=1.25),
-    IconText(152, "lon", center=True),  # "Lon" ???
+    IconSvg(152, "longitude"),  # Longitude ("Lon")
     IconText(153, "ft/s", scale=0.4),  # Speed Unit (ft/s)
     IconText(154, "a"),  # Amps Unit
-    IconText(155, "on\nmn", scale=0.4),  # Timer (On) TODO: Clock graphic?
-    IconText(156, "fly\nmn", scale=0.4),  # Timer (On) TODO: Drone clock graphic?
+    IconSvg(155, "airplane-clock"),  # Timer (On) TODO: Clock graphic?
+    IconSvg(156, "battery-clock"),  # Timer (On) TODO: Drone clock graphic?
     IconText(157, "mph", scale=0.4),  # Speed Unit (mph)
     IconText(158, "kph", scale=0.4),  # Speed Unit (kph)
     IconText(159, "m/s", scale=0.4),  # Speed Unit (m/s)
@@ -152,54 +261,29 @@ def get_path_to_material_icon(
 
 
 def get_icon_image(icon: IconImage) -> Image.Image:
+    print(icon)
     if isinstance(icon, IconEmpty):
         return Image.new("RGBA", (TILE_WIDTH, TILE_HEIGHT), (0, 0, 0, 0))
-    elif isinstance(icon, IconText):
+    else:
         return icon.generate()
-    elif isinstance(icon, IconCustom):
-        return icon.generator()
 
-    out_img = Image.new("RGBA", (TILE_WIDTH, TILE_HEIGHT), (0, 0, 0, 0))
 
-    icon_path = get_path_to_material_icon(icon.material_icon)
-
-    icon_img = Image.open(cast(str, icon_path))
-    icon_img = icon_img.convert("RGBA")
-    icon_img_a = icon_img.getchannel("A")
-    icon_img_l = Image.new("L", icon_img.size, 255)
-    icon_img = Image.merge(
-        "LA",
-        (
-            icon_img_l,
-            icon_img_a,
-        ),
+def get_arrows_image() -> Image.Image:
+    img = Image.new(
+        "RGBA",
+        (TILE_WIDTH * TILES_PER_ROW, TILE_HEIGHT),
+        (0, 0, 0, 0),
     )
 
-    icon_img = icon_img.resize(
-        (
-            int(TILE_WIDTH * icon.scale),
-            int(TILE_WIDTH * icon.scale),
-        ),
-        resample=Image.Resampling.BILINEAR,
-    )
+    arrow_icon = IconSvg(0, "arrow-down-bold").generate()
 
-    scale_width_diff = TILE_WIDTH - icon_img.width
-    scale_height_diff = TILE_HEIGHT - icon_img.height
+    for i in range(0, TILES_PER_ROW):
+        rotated_arrow_icon = arrow_icon.rotate(
+            i * 22.5, resample=Image.Resampling.BICUBIC
+        )
+        img.paste(rotated_arrow_icon, (i * TILE_WIDTH, 0))
 
-    paste_x = scale_width_diff // 2
-    paste_y = scale_height_diff // 2
-
-    out_img.paste(
-        icon_img,
-        (paste_x, paste_y),
-        icon_img,
-    )
-
-    if icon.with_background:
-        euclid = euclidean_distance_transform(out_img.getchannel("A"))
-        out_img = add_outline_via_euclidean_distance_transform(out_img, euclid)
-
-    return out_img
+    return img
 
 
 def get_char_image() -> Image.Image:
@@ -338,6 +422,9 @@ def main():
 
     chars_img = get_char_image()
     out.paste(chars_img, (0, TILE_HEIGHT * 2))
+
+    arrows_img = get_arrows_image()
+    out.paste(arrows_img, (0, TILE_HEIGHT * 6))
 
     for icon in ICONS:
         icon_img = get_icon_image(icon)
